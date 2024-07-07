c6dc031c6e65ffbee2113b74ad7d6c00828b4c4a
narus885 <narudon.song@gmail.com>
2024-07-07 20:11:32 +0700

    Fix path css

--- Frontend/quiz-app/src/pages/MainPage.tsx
+++ Frontend/quiz-app/src/pages/MainPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import './styles.css';
+import '../styles.css';
 
 const MainPage: React.FC = () => {
     const navigate = useNavigate();
